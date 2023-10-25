cd8d70384ecd1e247f70be5374d5bcf526bd7f89
NotCruz42 <146401617+NotCruz42@users.noreply.github.com>
2023-10-25 12:32:27 -0700

    Graphing_raw_data.py

--- 370 project.py
+++ 370 project.py
@@ -1,25 +1,38 @@
+import re
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Sample EEG data
-data = [
-    [0.4, -0.40000038147009037, 0.03477939107836826, -0.4],
-    [0.4, -0.40000038147009037, 0.04727902152921823, -0.4],
-    [0.4, -0.40000038147009037, 0.050899554156831894, -0.4],
-    [0.4, -0.40000038147009037, 0.0510742674582171, -0.4],
-    [0.4, -0.40000038147009037, 0.05039105452638104, -0.4],
-    [0.4, -0.3875007510192404, 0.04942822401831057, -0.39642905848413323]
-]
+# Define a regular expression pattern to match the data in each row
+pattern = r"O1=([-+]?\d*\.\d+|\d+), O2=([-+]?\d*\.\d+|\d+), T3=([-+]?\d*\.\d+|\d+), T4=([-+]?\d*\.\d+|\d+)"
+
+# Initialize lists to store the EEG data for each electrode
+o1_data = []
+o2_data = []
+t3_data = []
+t4_data = []
+
+# Open and read the CSV file
+with open('HeadbandData.csv', 'r') as file:
+    for line in file:
+        # Use regex to find EEG data in each row
+        match = re.search(pattern, line)
+        if match:
+            # Extract EEG amplitudes for each electrode
+            o1, o2, t3, t4 = map(float, match.groups())
+            o1_data.append(o1)
+            o2_data.append(o2)
+            t3_data.append(t3)
+            t4_data.append(t4)
 
 # Create time points for the x-axis
-time_points = np.arange(len(data))
+time_points = np.arange(len(o1_data))
 
 # Plot EEG data for each electrode
 plt.figure(figsize=(12, 6))
-plt.plot(time_points, [d[0] for d in data], label='O1')
-plt.plot(time_points, [d[1] for d in data], label='O2')
-plt.plot(time_points, [d[2] for d in data], label='T3')
-plt.plot(time_points, [d[3] for d in data], label='T4')
+plt.plot(time_points, o1_data, label='O1')
+plt.plot(time_points, o2_data, label='O2')
+plt.plot(time_points, t3_data, label='T3')
+plt.plot(time_points, t4_data, label='T4')
 
 # Add labels and legend
 plt.xlabel('Time')
@@ -29,3 +42,5 @@ plt.legend()
 
 # Show the plot
 plt.show()
+
+
